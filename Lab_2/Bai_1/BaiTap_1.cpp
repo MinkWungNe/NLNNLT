be5c877d2af16dbac8cc548fd80e6896ebe94f31
MinhWungNe <leluongminhquan@gmail.com>
2024-03-04 11:33:22 +0700

    #

--- Lab_2/Bai_1/BaiTap_1.cpp
+++ Lab_2/Bai_1/BaiTap_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+//prototype bool chiahetKQ kiemtrachiahet(int a, int b)
 
 int main()
 {
@@ -16,5 +17,22 @@ int main()
     {
         printf("Ket qua: a khong chia het cho b");
     }
+    return 0;
+}
+
+// Sửa bài
+
+bool kiemtrachiahet (int a,int b)
+{
+    bool chiahetKQ = a % b == 0 ? a + "chia het cho" + b:a +"false"+ b;
+    return chiahetKQ;
+}
+
+int main()
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << kiemtrachiahet(a, b);
     return 0;
 }
