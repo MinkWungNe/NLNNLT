0e3c3a702d479875b40c1e5155e2cf52244b32bb
MinkWungNe <leluongminhquan@gmail.com>
2024-03-11 09:21:25 +0700

    Lab_1

---
+++ Lab_1/Bai_1/BaiTap_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cout << "Nhap a: "; cin >> a;
+    cout << "Nhap b: "; cin >> b;
+    cout << "Nhap c: "; cin >> c;
+    d = a + b +c;
+    cout << "Tong d: " << d << endl;
+    return 0;
+}
