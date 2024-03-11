bedb2290a4ccfd7903322c0937ab550f481c4ee2
MinkWungNe <leluongminhquan@gmail.com>
2024-03-11 09:54:13 +0700

    added FlowCharts

---
+++ Lab_1/Bai_2/BaiTap_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float R, P;
+    const float PI = 3.14;
+    cout << "Nhap ban kinh R: "; cin >> R;
+    P = 2*PI*R;
+    cout << "Chu vi hinh tron: " << P << endl;
+}
