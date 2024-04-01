2f60ee834d10f33e7c011ca11bc33e4cac28ec8c
MinkWungNe <leluongminhquan@gmail.com>
2024-04-01 10:02:29 +0700

    Using for() instead of pow()

--- Lab_3/Bai_1/BaiTap_1_3.cpp
+++ Lab_3/Bai_1/BaiTap_1_3.cpp
@@ -1,6 +1,5 @@
 //Viết chương trình cho phép người dùng nhập vào hai số nguyên dương a và n. Sử dụng cấu trúc lặp đã học để tính a luỹ thừa n (a^n).
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
@@ -10,7 +9,14 @@ int main()
     cin >> a;
     cout << "Nhap n: ";
     cin >> n;
-    int KetquaLuythua = pow(a,n);
+    //int KetquaLuythua = pow(a,n);
+    int KetquaLuythua;
+    for (int i = 0; i < n ; i++)
+    {
+        int KetquaLuythua = a*i;
+    }
     cout << "Ket qua: " << KetquaLuythua <<endl;
     return 0;
 }
+
+// không nên dùng pow() vì với các bài toán cơ bản dùng pow() sẽ tiêu tốn tài nguyên ko cần thiết. Dùng for() để lặp sẽ cho kết quả nhanh hơn.
